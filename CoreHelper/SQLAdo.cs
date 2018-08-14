a2e1108171ec477c75fc4d684c6d8623c24769c9
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-08-14 17:59:07 +0100

    Updated Solution Name and added

---
+++ CoreHelper/SQLAdo.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace CoreHelpers
+{
+    /// <summary>
+    /// SQL Server and ADO.NET
+    /// </summary>
+    public class SQLAdo
+    {
+        protected string ConnectionString { get; set; }
+
+        public SQLAdo(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(this.ConnectionString);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            return connection;
+        }
+
+        protected DbCommand GetCommand(DbConnection connection, string commandText, CommandType commandType)
+        {
+            SqlCommand command = new SqlCommand(commandText, connection as SqlConnection);
+            command.CommandType = commandType;
+            return command;
+        }
+
+        public SqlParameter GetParameter(string parameter, object value)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, value != null ? value : DBNull.Value);
+            parameterObject.Direction = ParameterDirection.Input;
+            return parameterObject;
+        }
+
+        public SqlParameter GetTableValueParameter(string parameter, object value)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, value != null ? value : DBNull.Value);
+            parameterObject.Direction = ParameterDirection.Input;
+            parameterObject.SqlDbType = SqlDbType.Structured;
+            return parameterObject;
+        }
+
+        public SqlParameter GetParameterOut(string parameter, SqlDbType type, object value = null, ParameterDirection parameterDirection = ParameterDirection.InputOutput)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, type); ;
+
+            if (type == SqlDbType.NVarChar || type == SqlDbType.VarChar || type == SqlDbType.NText || type == SqlDbType.Text)
+            {
+                parameterObject.Size = -1;
+            }
+
+            parameterObject.Direction = parameterDirection;
+
+            if (value != null)
+            {
+                parameterObject.Value = value;
+            }
+            else
+            {
+                parameterObject.Value = DBNull.Value;
+            }
+
+            return parameterObject;
+        }
+
+        public int ExecuteNonQuery(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            int returnValue = -1;
+
+            try
+            {
+                using (SqlConnection connection = this.GetConnection())
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return returnValue;
+        }
+
+        public object ExecuteScalar(string procedureName, List<SqlParameter> parameters)
+        {
+            object returnValue = null;
+
+            try
+            {
+                using (DbConnection connection = this.GetConnection())
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, CommandType.StoredProcedure);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return returnValue;
+        }
+
+        public DbDataReader GetDataReader(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            DbDataReader ds;
+
+            try
+            {
+                DbConnection connection = this.GetConnection();
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    ds = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return ds;
+        }
+    }
+
+    public static class Mapper
+    {
+        public static T Map<T>(this T @class, DbDataReader reader)
+        {
+            PropertyInfo[] properties = @class.GetType().GetProperties();
+
+            while (reader.Read())
+            {
+                foreach (PropertyInfo property in properties)
+                {
+                    if (reader[property.Name.ToString()] != null)
+                    {
+                        property.SetValue(@class, reader[property.Name.ToString()]);
+                    }
+                }
+            }
+            return @class;
+        }
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using ClassHelpers;
+﻿using CoreHelpers;
 using System;
 using System.Collections.Generic;
 using System.Data;

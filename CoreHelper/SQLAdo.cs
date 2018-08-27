30dbf53ccfdd469e84cd741590321806aa0a6dd5
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-08-27 10:02:03 +0100

    Update

--- CoreHelper/SQLAdo.cs
+++ CoreHelper/SQLAdo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 namespace CoreHelpers
 {
     /// <summary>
-    /// SQL Server and ADO.NET
+    /// ADO.NET helper
     /// </summary>
     public class SQLAdo
     {
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Reflection;
 using TestConsole.Models;
 
 namespace TestConsole
